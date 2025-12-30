867949446adf3069a77a4431d9de7758a790f8ae
Anglebase <1628818324@qq.com>
2025-12-30 17:55:42 +0800

    重构代码结构

--- src/buildcpp/const.py
+++ src/buildcpp/const.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 
+
 def _get_root_dir() -> tuple[Path, str]:
     main = sys.modules['__main__']
     assert main.__file__ is not None, "Internal error: no __file__ attribute in __main__ module"
@@ -12,4 +13,4 @@ def _get_root_dir() -> tuple[Path, str]:
 
 PROJECT_ROOT, PROJECT_NAME = _get_root_dir()  # Project root directory
 BUILD_ROOT = Path(PROJECT_ROOT) / 'build'  # Build directory
-SRC_ROOT = Path(PROJECT_ROOT) /'src'  # Source directory
+SRC_ROOT = Path(PROJECT_ROOT) / 'src'  # Source directory
