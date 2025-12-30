fa8e678fedc77148ef882aca026b136f0f2c3e97
Anglebase <1628818324@qq.com>
2025-12-30 18:47:59 +0800

    添加CMake检查

--- src/buildcpp/cmake/build.py
+++ src/buildcpp/cmake/build.py
@@ -2,11 +2,26 @@ from pathlib import Path
 from .target import AbstractTarget
 from ..const import PROJECT_NAME, BUILD_ROOT
 import os
+import subprocess
 
 
 class Builder:
-    def __init__(self) -> None:
+    def __init__(self, *, cmake=None) -> None:
         self.targets = []
+
+        self._cmake = cmake or "cmake"
+        self._check_cmake()
+
+    def _check_cmake(self):
+        result = subprocess.run(
+            [self._cmake, "--version"],
+            capture_output=True,
+            shell=True,
+            text=True,
+        )
+        assert result.returncode == 0, "CMake not found"
+        stdout = result.stdout.strip()
+        assert "cmake version" in stdout, "Invalid CMake"
 
     def attach(self, target: AbstractTarget):
         self.targets.append(target)
@@ -25,5 +40,5 @@ class Builder:
     def build(self, *, output_dir: Path = BUILD_ROOT):
         self._gen_cmakelists(output_dir)
         os.system(
-            f"cmake -S {output_dir.as_posix()} -B {output_dir.as_posix()}")
-        os.system(f"cmake --build {output_dir.as_posix()}")
+            f"{self._cmake} -S {output_dir.as_posix()} -B {output_dir.as_posix()}")
+        os.system(f"{self._cmake} --build {output_dir.as_posix()}")
