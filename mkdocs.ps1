c983719de514d0d663c22ddab8d5c31e72558bf9
Anglebase <1628818324@qq.com>
2026-01-02 13:10:34 +0800

    移除文件

--- mkdocs.ps1
+++
@@ -1,9 +0,0 @@
-pdoc --html --output-dir dist src/buildcpp
-mkdocs build --site-dir dist/site
-git checkout site
-Move-Item -Path dist/site -Destination ./ -Force
-Move-Item -Path dist/buildcpp -Destination docs -Force
-git stash save
-git stash list
-git commit -m "Update documentation"
-git checkout master
